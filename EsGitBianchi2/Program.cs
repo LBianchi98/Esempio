49bc54731e613df5194d896ac42711b9207955cf
Lorenzo Bianchi <lorenzo.bianchi0210@gmail.com>
2018-11-05 12:16:30 +0100

    Massimo e Minimo array

---
+++ EsGitBianchi2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] v = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                v[i] = r.Next(50);
+            }
+            //Array.Sort(v);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(v[i] + " ");
+            }
+            Console.Write("\n");
+            Console.WriteLine("Massimo: " + v.Max());
+            Console.WriteLine("Minimo:" + v.Min());
+            Console.ReadLine();
+        }
+    }
+}
